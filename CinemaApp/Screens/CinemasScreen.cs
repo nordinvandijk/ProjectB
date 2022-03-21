cd87531f7ab02f5b6b53040eba988f136dc89eb1
Leon Bleuanus <bleujr@hotmail.nl>
2022-03-21 20:01:59 +0100

    Bioscopen toegevoegd

--- CinemaApp/Screens/CinemasScreen.cs
+++ CinemaApp/Screens/CinemasScreen.cs
@@ -15,7 +15,11 @@ namespace CinemaApp.Screens
         //Methods
         public override void run()
         {
-            Console.WriteLine("Dit is een testbericht");
+            string[] bioscoopnaam = new string[]{"De BIOS Rotterdam", "De BIOS Den Haag", "De BIOS Amsterdam", "De BIOS Utrecht", "De BIOS Eindhoven"};
+            string[] adres = new string[]{"Ahoyweg 12, 3084 BA Rotterdam", "President Kennedylaan 10, 2517 JK Den Haag", "Javakade 30, 1019 SZ Amsterdam", "Van Deventerlaan 20, 3528 AE Utrecht", "Willemstraat 40, 5616 GE Eindhoven"};
+            for(int i = 0; i < bioscoopnaam.Length && i < adres.Length; i++){
+                Console.WriteLine($"- {bioscoopnaam[i]} \n  {adres[i]}");
+            }
         }
     }   
 }
