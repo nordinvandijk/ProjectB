d35d092e05504338df5a02e32002f7841496c010
Nordin van Dijk <nordindeechte@gmail.com>
2022-03-22 00:05:16 +0100

    Update MovieManager.cs
    - Added xml documentation
    - Added RemoveMovie() method
    - Added UpdateJson() method

--- CinemaApp/MovieManager.cs
+++ CinemaApp/MovieManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using System.Linq;
 using static System.Console;
 
 namespace CinemaApp
@@ -8,14 +9,23 @@ namespace CinemaApp
     class MovieManager
     {
         public List<Movie> movies = new List<Movie>();
-
+        
         public MovieManager() {
             LoadJson(); // zorgt ervoor dat de list "movies" direct na het runnen gevuld wordt met de movies van de json
         }
 
+        /// <summary>
+        /// This method will first make a new Movie object with all the required parameters you need to pass in. Then it adds the Movie object to the List<Movie> called "movies"
+        /// and at last it will update the json file called "movieList" by using UpdateJson()
+        /// </summary>
+        /// <param name="title">Tile of the movie you want to add.</param>
+        /// <param name="desc">Description of the movie you want to add.</param>
+        /// <param name="releaseDate">Release date of the movie you want to add.</param>
+        /// <param name="genre">Genre of the movie you want to add.</param>
+        /// <param name="minAge">Minimum age of the movie you want to add.</param>
+        /// <param name="kijkwijzer">The dutch "kijkwijzer" of the movie you want to add.</param>
         public void AddMovie(string title, string desc, string releaseDate, string[] genre, int minAge, string[] kijkwijzer) 
         {
-            // Maakt een movie object aan met de meegegeven arguments van "AddMovie"
             Movie mov = new Movie() {
                 Title = title,
                 Description = desc,
@@ -24,21 +34,26 @@ namespace CinemaApp
                 MinimumAge = minAge,
                 Kijkwijzer = kijkwijzer
             };
-            // <<<
-
-            // Voegt het gemaakt object toe aan de "Movie" list
             movies.Add(mov);
-            
-            // Deze blok code zorgt ervoor dat de nieuw toegevoegde film ook direct in het json bestand wordt gestopt
-            using (StreamWriter sw = new StreamWriter("movieList.json"))
-            {   
-                string json = JsonConvert.SerializeObject(movies,Formatting.Indented);
-                sw.WriteLine(json);
-            }
-            // <<<
+            UpdateJson();
         }
 
+        /// <summary>
+        /// Removes a movie based on title from the List<Movie> called "movies".
+        /// </summary>
+        /// <param name="title">The title of the movie you want to delete.</param>
+        public void RemoveMovie(string title) {
+            foreach (Movie mov in movies.ToList()) { // Mischien handig om hier een reverse for loop te gebruiken ipv de list tijdelijk te kopieeren met ToList().
+                if (mov.Title == title) {
+                    movies.Remove(mov);
+                }
+            }
+            UpdateJson();
+        }
 
+        /// <summary>
+        /// Loads all json objects from "movieList.json" in. Converts the json objects to Movie.cs objects and those Movie objects get stored in the List<Movie> called "movies".
+        /// </summary>
         public void LoadJson() 
         {
             using (StreamReader sr = new StreamReader("movieList.json"))
@@ -46,6 +61,18 @@ namespace CinemaApp
                 string json = sr.ReadToEnd();
                 movies = JsonConvert.DeserializeObject<List<Movie>>(json);
             }
+        }
+
+        /// <summary>
+        /// Updates the file "movieList.json" by writing all the Movie classes inside List<Movie> called "movies" to "movieList.json".
+        /// </summary>
+        public void UpdateJson() 
+        {
+           using (StreamWriter sw = new StreamWriter("movieList.json"))
+            {   
+                string json = JsonConvert.SerializeObject(movies,Formatting.Indented);
+                sw.WriteLine(json);
+            } 
         }
     }
 }
